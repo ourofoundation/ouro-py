b278bffb38dc9bc11790c31ab85d51666a75c707
Matt Moderwell <mmoderwell@gmail.com>
2024-01-14 11:25:05 -0800

    Foundational work on Chronos' blueprints

--- ouro/__init__.py
+++ ouro/__init__.py
@@ -1,10 +1,25 @@
+import logging
 import os
+import time
 
 import requests
 from dotenv import load_dotenv
 from supabase.client import ClientOptions
 
 from supabase import Client, create_client
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+formatter = logging.Formatter(log_format)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+postgres_logger = logging.getLogger("httpx")
+postgres_logger.setLevel(logging.WARNING)
+
 
 load_dotenv()
 
@@ -42,3 +57,32 @@ class Ouro:
 
         user = self.client.auth.get_user(token).user
         print(f"Successfully logged in as {user.email}.")
+
+    def load_dataset(self, table_name: str, schema: str = "datasets"):
+        start = time.time()
+
+        row_count = self.client.table(table_name).select("*", count="exact").execute()
+        row_count = row_count.count
+
+        logger.info(f"Loading {row_count} rows from {schema}.{table_name}...")
+        # Batch load the data if it's too big
+        if row_count > 1_000_000:
+            data = []
+            for i in range(0, row_count, 1_000_000):
+                logger.debug(f"Loading rows {i} to {i+1_000_000}")
+                res = (
+                    self.client.table(table_name)
+                    .select("*")
+                    .range(i, i + 1_000_000)
+                    .execute()
+                )
+                data.extend(res.data)
+        else:
+            res = self.client.table(table_name).select("*").limit(1_000_000).execute()
+            data = res.data
+
+        end = time.time()
+        logger.info(f"Finished loading data in {round(end - start, 2)} seconds.")
+
+        self.data = data
+        return data
