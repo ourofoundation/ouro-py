6e2a6f78b33235075c957a5887349dca393e7c9a
Matt Moderwell <mmoderwell@gmail.com>
2024-05-20 09:46:39 -0700

    Fix non-https version of backend url

--- ouro/config.py
+++ ouro/config.py
@@ -13,4 +13,4 @@ class Config:
         "SUPABASE_ANON_KEY",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
     )
-    OURO_BACKEND_URL = os.getenv("OURO_BACKEND_URL", "http://api.ouro.foundation")
+    OURO_BACKEND_URL = os.getenv("OURO_BACKEND_URL", "https://api.ouro.foundation")
