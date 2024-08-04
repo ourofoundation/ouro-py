a49091c4acca3d6fb29835917cc3df69c4a2a284
Matt Moderwell <mmoderwell@gmail.com>
2024-08-04 13:08:23 -0700

    Update anon key for new deployment

--- ouro/__version__.py
+++ ouro/__version__.py
@@ -1,2 +1,2 @@
 __title__ = "ouro"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- ouro/config.py
+++ ouro/config.py
@@ -11,6 +11,6 @@ class Config:
     SUPABASE_URL = os.getenv("SUPABASE_URL", "https://database.ouro.foundation")
     SUPABASE_ANON_KEY = os.getenv(
         "SUPABASE_ANON_KEY",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzIyNjY4NDAwLAogICJleHAiOiAxODgwNDM0ODAwCn0.48r75lJf_ByLzHb8GccNkW9qAhfrY0OIDUf40Jc1OsA",
     )
     OURO_BACKEND_URL = os.getenv("OURO_BACKEND_URL", "https://api.ouro.foundation")
